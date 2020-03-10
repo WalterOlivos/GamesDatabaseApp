d034dfae7807edbaf37d22f77e7c3feb6a314830
Walter <ws1olivos@gmail.com>
2020-03-10 10:18:11 -0300

    First Commit

---
+++ TCC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TCC
+//
+//  Created by Walter Oliveira on 10/03/20.
+//  Copyright © 2020 Walter Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
